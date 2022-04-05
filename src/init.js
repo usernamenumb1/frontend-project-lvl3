4f3e5da13f5eb3b140fa71c249e6206bb27f2e85
ivanleonov <treska1315@gmail.com>
2022-04-05 12:00:11 +0300

    deleted global object

--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
 import whatchState from './view.js';
-import elements from './elements.js';
+// import elements from './elements.js';
 import resources from './locales/index.js';
 import renders from './renders.js';
 import get from './requests.js';
@@ -29,6 +29,28 @@ export default () => {
         currentArticle: null,
         readedArticles: [],
       }, i18n);
+
+      const elements = {
+        input: document.querySelector('#input-url'),
+        inputLabel: document.querySelector('#input-label'),
+        form: document.querySelector('form'),
+        h1: document.querySelector('h1'),
+        feeds: document.querySelector('.feeds'),
+        posts: document.querySelector('.posts'),
+        paragraphs: {
+          leed: document.querySelector('p.leed'),
+          example: document.querySelector('p#example'),
+          feedBack: document.querySelector('p.feedback'),
+        },
+        submitButton: document.querySelector('form>div.row>div>button'),
+        modal: {
+          pane: document.querySelector('div.modal'),
+          header: document.querySelector('h5.modal-title'),
+          paragraph: document.querySelector('div>div.modal-body>p'),
+          link: document.querySelector('div.modal-footer>a'),
+          closeButton: document.querySelector('div.modal-footer>button'),
+        },
+      };
 
       elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
