1b12495d49ef1f96e9f033a87c2118d9fe4614be
ivanleonov <treska1315@gmail.com>
2022-04-05 13:13:09 +0300

    deleted global object 4th try

--- src/renders.js
+++ src/renders.js
@@ -1,26 +1,26 @@
 // import elements from './elements.js';
 
-const elements = {
-  input: document.querySelector('#input-url'),
-  inputLabel: document.querySelector('#input-label'),
-  form: document.querySelector('form'),
-  h1: document.querySelector('h1'),
-  feeds: document.querySelector('.feeds'),
-  posts: document.querySelector('.posts'),
-  paragraphs: {
-    leed: document.querySelector('p.leed'),
-    example: document.querySelector('p#example'),
-    feedBack: document.querySelector('p.feedback'),
-  },
-  submitButton: document.querySelector('form>div.row>div>button'),
-  modal: {
-    pane: document.querySelector('div.modal'),
-    header: document.querySelector('h5.modal-title'),
-    paragraph: document.querySelector('div>div.modal-body>p'),
-    link: document.querySelector('div.modal-footer>a'),
-    closeButton: document.querySelector('div.modal-footer>button'),
-  },
-};
+// const elements = {
+//   input: document.querySelector('#input-url'),
+//   inputLabel: document.querySelector('#input-label'),
+//   form: document.querySelector('form'),
+//   h1: document.querySelector('h1'),
+//   feeds: document.querySelector('.feeds'),
+//   posts: document.querySelector('.posts'),
+//   paragraphs: {
+//     leed: document.querySelector('p.leed'),
+//     example: document.querySelector('p#example'),
+//     feedBack: document.querySelector('p.feedback'),
+//   },
+//   submitButton: document.querySelector('form>div.row>div>button'),
+//   modal: {
+//     pane: document.querySelector('div.modal'),
+//     header: document.querySelector('h5.modal-title'),
+//     paragraph: document.querySelector('div>div.modal-body>p'),
+//     link: document.querySelector('div.modal-footer>a'),
+//     closeButton: document.querySelector('div.modal-footer>button'),
+//   },
+// };
 
 const createCard = (colType, listContent) => {
   const cardDiv = document.createElement('div');
@@ -52,8 +52,8 @@ const renderAll = (state, i18n) => {
 
 const renderFeedBack = (state, i18n) => {
   document.querySelector('p.feedback').classList.replace('text-success', 'text-danger');
-  if (state.error !== 'noError') elements.input.classList.add('is-invalid');
-  else elements.input.classList.remove('is-invalid');
+  if (state.error !== 'noError') document.querySelector('#input-url').classList.add('is-invalid');
+  else document.querySelector('#input-url').classList.remove('is-invalid');
   document.querySelector('p.feedback').textContent = i18n.t(`paragraphs.feedBack.errorMassages.${state.error}`);
 };
 
