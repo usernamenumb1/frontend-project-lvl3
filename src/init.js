49ee0cb66bf66a9487381224d6ae470886f8ef55
ivanleonov <treska1315@gmail.com>
2022-04-05 18:27:42 +0300

    try to reset input

--- src/init.js
+++ src/init.js
@@ -53,7 +53,7 @@ export default () => {
         },
       };
 
-      elements.form.addEventListener('submit', (e) => {
+      document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const url = formData.get('url').trim();
