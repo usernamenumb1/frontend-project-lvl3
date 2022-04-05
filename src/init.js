a470f4243ba2b760b77924e964088e5c8b8d34e1
ivanleonov <treska1315@gmail.com>
2022-04-05 18:42:26 +0300

    try to reset input 2nd attempt

--- src/init.js
+++ src/init.js
@@ -31,39 +31,36 @@ export default () => {
         readedArticles: [],
       }, i18n);
 
-      const elements = {
-        input: document.querySelector('#input-url'),
-        inputLabel: document.querySelector('#input-label'),
-        form: document.querySelector('form'),
-        h1: document.querySelector('h1'),
-        feeds: document.querySelector('.feeds'),
-        posts: document.querySelector('.posts'),
-        paragraphs: {
-          leed: document.querySelector('p.leed'),
-          example: document.querySelector('p#example'),
-          feedBack: document.querySelector('p.feedback'),
-        },
-        submitButton: document.querySelector('form>div.row>div>button'),
-        modal: {
-          pane: document.querySelector('div.modal'),
-          header: document.querySelector('h5.modal-title'),
-          paragraph: document.querySelector('div>div.modal-body>p'),
-          link: document.querySelector('div.modal-footer>a'),
-          closeButton: document.querySelector('div.modal-footer>button'),
-        },
-      };
-
-      document.querySelector('form').addEventListener('submit', (e) => {
+      // const elements = {
+      //   input: document.querySelector('#input-url'),
+      //   inputLabel: document.querySelector('#input-label'),
+      //   form: document.querySelector('form'),
+      //   h1: document.querySelector('h1'),
+      //   feeds: document.querySelector('.feeds'),
+      //   posts: document.querySelector('.posts'),
+      //   paragraphs: {
+      //     leed: document.querySelector('p.leed'),
+      //     example: document.querySelector('p#example'),
+      //     feedBack: document.querySelector('p.feedback'),
+      //   },
+      //   submitButton: document.querySelector('form>div.row>div>button'),
+      //   modal: {
+      //     pane: document.querySelector('div.modal'),
+      //     header: document.querySelector('h5.modal-title'),
+      //     paragraph: document.querySelector('div>div.modal-body>p'),
+      //     link: document.querySelector('div.modal-footer>a'),
+      //     closeButton: document.querySelector('div.modal-footer>button'),
+      //   },
+      // };
+      const form = document.querySelector('form');
+      const input = document.querySelector('#input-url');
+      form.addEventListener('submit', (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const url = formData.get('url').trim();
         schema.notOneOf(state.urlList, 'alreadyExists').validate(url)
           .then(() => get.feedData(url))
           .then((parsedRss) => utils.updateState(parsedRss, state, url))
-          .then(() => {
-            elements.form.reset();
-            elements.input.focus();
-          })
           .then(() => get.refreshFeedData(url, state))
           .catch((err) => {
             switch (err.message) {
@@ -75,8 +72,11 @@ export default () => {
             }
           });
         state.loadingStatus = 'filling';
+        form.reset();
+        input.focus();
       });
-      elements.posts.addEventListener('click', (e) => {
+      const posts = document.querySelector('.posts');
+      posts.addEventListener('click', (e) => {
         if (e.target.type) {
           const targetId = e.target.dataset.id;
           const link = document.querySelector(`a[data-id='${targetId}']`);
